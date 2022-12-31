d922577178178a84dcc39050777a88e1233a1f4e
Lena Niku <niku.elena@gmail.com>
2022-12-31 23:22:42 +0300

    add tests

---
+++ src/MagicTest.java
@@ -0,0 +1,32 @@
+import org.junit.jupiter.api.*;
+
+public class MagicTest {
+
+    @Test
+    public void testRTE() {
+        MagicBox<Integer> magicBox = new MagicBox<>(1);
+        Assertions.assertThrows(RuntimeException.class, () -> magicBox.pick());
+    }
+
+    @Test
+    public void testAddFunction() {
+        MagicBox<Integer> magicBox = new MagicBox<>(1);
+        boolean result = magicBox.add(1);
+        Assertions.assertEquals(true, result);
+    }
+
+    @Test
+    public void testConstructor() {
+        MagicBox<Integer> magicBox = new MagicBox<>(9);
+        Assertions.assertNotNull(magicBox);
+    }
+
+    @Test
+    public void testPickFunction() {
+        MagicBox<Integer> magicBox = new MagicBox<>(1);
+        magicBox.add(111);
+        Integer result = magicBox.pick();
+        Assertions.assertEquals(111, result);
+    }
+
+}
